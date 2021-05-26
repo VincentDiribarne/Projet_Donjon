4a5dda596e1919dc94ab76666d5e92535bcbd237
Vincent Diribarne <vdiribarnepro@gmail.com>
2021-05-26 16:34:52 +0200

    Commentaire pour De et Library

--- code/src/model/De.java
+++ code/src/model/De.java
@@ -4,11 +4,22 @@ import java.util.Random;
 
 public class De {
 
+    /**
+     * Generate a random number
+     * @param borneInf
+     * @param borneSup
+     * @return int
+     */
     public static int genererInt(int borneInf, int borneSup){
         Random random = new Random();
         return borneInf+random.nextInt((borneSup-borneInf) +1);
     }
 
+    /**
+     * Generate a number like a dice
+     * @param n
+     * @return int
+     */
     public static int lancerDes(int n){
         Random random = new Random();
         return random.nextInt((n-1) +1) + 1;
